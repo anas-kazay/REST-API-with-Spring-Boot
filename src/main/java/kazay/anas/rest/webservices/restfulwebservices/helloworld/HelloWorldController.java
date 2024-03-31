701a1afd882b150665270f1f5efc64a1e1d97dbd
anas-kazay <anaskazay@gmail.com>
2024-03-31 15:18:24 +0000

    first commit

---
+++ src/main/java/kazay/anas/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,20 @@
+package kazay.anas.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
+}
